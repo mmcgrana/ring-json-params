cb5a6baa5f6a5454e3aea579fc635c86029cab27
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-04 08:20:33 -0700

    add tests

---
+++ test/ring/middleware/json_params_test.clj
@@ -0,0 +1,28 @@
+(ns ring.middleware.json-params-test
+  (:use clojure.test)
+  (:use ring.middleware.json-params)
+  (:require [clojure.contrib.io :as io])
+  (:import java.io.ByteArrayInputStream))
+
+(defn stream [s]
+  (ByteArrayInputStream. (.getBytes s "UTF-8")))
+
+(def json-echo
+  (wrap-json-params identity))
+
+(deftest noop-with-other-content-type
+  (let [req {:content-type "application/xml"
+             :body (stream "<xml></xml>")
+             :params {"id" 3}}
+        resp (json-echo req)]
+    (is (= "<xml></xml>") (io/slurp* (:body resp)))
+    (is (= {"id" 3} (:params resp)))
+    (is (nil? (:json-params resp)))))
+
+(deftest augments-with-json-content-type
+  (let [req {:content-type "application/json; charset=UTF-8"
+             :body (stream "{\"foo\": \"bar\"}")
+             :params {"id" 3}}
+        resp (json-echo req)]
+    (is (= {"id" 3 "foo" "bar"} (:params resp)))
+    (is (= {"foo" "bar"} (:json-params resp)))))
