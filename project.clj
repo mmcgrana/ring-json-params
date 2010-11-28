d291536657ffb5db2b51654f135698821d8acf60
Mark McGranaghan <mmcgrana@gmail.com>
2010-11-28 15:30:40 -0800

    release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-json-params "0.1.1"
+(defproject ring-json-params "0.1.2"
   :description "Ring middleware for JSON params parsing."
   :dependencies
    [[org.clojure/clojure "1.2.0"]
