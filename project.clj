ef84fc06fac96be7f071d43e330411bfad14680c
webus <webusnix@gmail.com>
2012-10-28 10:28:16 +0400

    get rid of the clojure.contrib

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject ring-json-params "0.1.4"
   :description "Ring middleware for JSON params parsing."
   :dependencies
-   [[org.clojure/clojure "1.4.0"]    
-    [cheshire "4.0.3"]])
+   [
+   	[org.clojure/clojure "1.4.0"]    
+    [cheshire "4.0.3"]
+   ]
+   :aot [ring.middleware.json-params])
